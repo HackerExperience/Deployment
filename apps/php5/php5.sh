2b1565d9248aaf561e217aab1f611e81e856d42c
Renato Massaro <renato@hackerexperience.com>
2016-01-23 18:47:02 -0200

    Work on PHP deployment script

--- apps/php5/php5.sh
+++ apps/php5/php5.sh
@@ -8,16 +8,14 @@ read_php5_input(){
     PHP_SRC_DIR='.'
     PHP_CONTAINER_NAME='php5'
     PHP_CONTAINER_IMAGE='he_php5'
-    PHP_PORT='9000'
     PHP_CONF_DIR='/php-conf'
 
     while test $# -gt 0; do
         case "$1" in
-            --php-src) full_path $2 && PHP_SRC_DIR ;;
+            --php-src) full_path $2 && PHP_SRC_DIR=$2 ;;
             --php-name) PHP_CONTAINER_NAME=$2 ;;
             --php-image) PHP_CONTAINER_IMAGE=$2 ;;
             --php-conf) full_path $2 && PHP_CONF_DIR=$2 ;;
-            --php-port) PHP_PORT=$2 ;;
             --skip-php) SKIP_PHP=1 ;;
         esac
         shift
@@ -40,25 +38,28 @@ deploy_php5(){
     mkdir -p $PHP_CONF_DIR
 
     install_config php.ini $PHP_SRC_DIR $PHP_CONF_DIR
-    install_config php-fpm.ini $PHP_SRC_DIR $PHP_CONF_DIR
+    install_config php-fpm.conf $PHP_SRC_DIR $PHP_CONF_DIR
     install_config conf.d $PHP_SRC_DIR $PHP_CONF_DIR
 
     docker run -d \
-        --name $PHP_CONTAINER_NAME \
-        -v ${PHP_CONF_DIR}/php.ini:/etc/php.ini:ro \
-        -v ${PHP_CONF_DIR}/php-fpm.ini:/etc/php-fpm.ini:ro \
-        -v ${PHP_CONF_DIR}/conf.d:/etc/php/conf.d:rw \
+        --name ${PHP_CONTAINER_NAME}_data \
+        -v ${PHP_CONF_DIR}:/etc/php:ro \
+        -v ${LOGS_DIR}/${PHP_CONTAINER_NAME}:/var/log/php \
         busybox /bin/true
 
     docker run -d \
         --name $PHP_CONTAINER_NAME \
         --volumes-from ${PHP_CONTAINER_NAME}_data \
-        -p $PHP_PORT:9000 \
         $PHP_CONTAINER_IMAGE
 
     sleep 2
 
-    verify_running NGINX_CONTAINER_NAME
+    docker run -d \
+        --name ${PHP_CONTAINER_NAME}_socket \
+        --volumes-from $PHP_CONTAINER_NAME \
+        busybox /bin/true
+
+    verify_running $PHP_CONTAINER_NAME
 
 }
 
