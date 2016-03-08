3e76d104f49111db01b4485346f98b29bbe9aa8a
Renato Massaro <renato@hackerexperience.com>
2016-03-08 10:13:45 -0300

    Add Jenkins deployment files

---
+++ apps/jenkins/jenkins.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+
+source $(dirname $0)/../../common.sh
+
+read_jenkins_input(){
+    
+    JENKINS_CONTAINER_NAME='jenkins'
+    JENKINS_DATA_DIR='/jenkins-data'
+    JENKINS_WEB_PORT='8080'
+
+    while test $# -gt 0; do
+        case "$1" in
+            --jenkins-src) full_path $2 && JENKINS_SRC_DIR=$2 ;;
+            --jenkins-name) JENKINS_CONTAINER_NAME=$2 ;;
+            --jenkins-data) full_path $2 && JENKINS_DATA_DIR=$2 ;;
+            --jenkins-web-port) JENKINS_WEB_PORT=$2 ;;
+        esac
+        shift
+    done
+
+}
+
+deploy_jenkins(){
+
+    read_common_input "$@"
+    read_jenkins_input "$@"
+
+    verify_conflict ${JENKINS_CONTAINER_NAME}_data
+
+    mkdir -p $JENKINS_DATA_DIR
+
+    docker run -d \
+        --name ${JENKINS_CONTAINER_NAME}_data \
+        -v ${JENKINS_DATA_DIR}:/var/jenkins_home:rw \
+        busybox /bin/true
+
+    # We need to have a `jenkins` user on the host due to volume permissions.
+    id -u jenkins &>/dev/null || useradd -MNs /dev/null jenkins
+
+    jenkins_uid=$(id -u jenkins)
+    chown -R jenkins_uid $JENKINS_DATA_DIR
+
+    docker run -d \
+        --name ${JENKINS_CONTAINER_NAME} \
+        --volumes-from ${JENKINS_CONTAINER_NAME}_data \
+        -p ${JENKINS_WEB_PORT}:8080 \
+        -u $(id -u jenkins)
+        jenkins
+
+}
+
+if [ "$0" = "$BASH_SOURCE" ]; then
+
+    deploy_jenkins "$@"
+
+fi
