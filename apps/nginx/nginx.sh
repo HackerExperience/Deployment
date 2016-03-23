7d955ed030590c9a7362cbef9e0d4a77a7255edf
Renato Massaro <renato@hackerexperience.com>
2016-03-23 03:24:43 -0300

    Add custom volumes support for nginx

--- apps/nginx/nginx.sh
+++ apps/nginx/nginx.sh
@@ -11,6 +11,7 @@ read_nginx_input(){
     NGINX_PORT='80'
     NGINX_CONF_DIR='/nginx-conf'
     NGINX_DATA_DIR='/nginx-data'
+    CUSTOM_VOLUMES=""
 
     while test $# -gt 0; do
         case "$1" in
@@ -20,11 +21,14 @@ read_nginx_input(){
             --nginx-port) NGINX_PORT=$2 ;;
             --nginx-conf) full_path $2 && NGINX_CONF_DIR=$2 ;;
             --nginx-data) full_path $2 && NGINX_DATA_DIR=$2 ;;
+            --no-nginx-data) NO_NGINX_DATA=1 ;;
             --php-socket) PHP_SOCKET_NAME=$2 && NGINX_USE_PHP=1 ;;
+            --nginx-custom-volume) CUSTOM_VOLUMES+="$2 " ;;
             --skip-nginx) SKIP_NGINX=1 ;;
         esac
         shift
     done
+
 }
 
 deploy_nginx(){
@@ -63,18 +67,30 @@ deploy_nginx(){
     # LAUNCH CONTAINERS
     ############################################################################
 
+    nginx_data=""
+    if [ -z $NO_NGINX_DATA ]; then
+        nginx_data+=" -v ${NGINX_DATA_DIR}:/var/www:rw "
+    fi
+
     docker run -d \
         --name ${NGINX_CONTAINER_NAME}_data \
         -v ${NGINX_CONF_DIR}/nginx.conf:/etc/nginx/nginx.conf:ro \
         -v ${NGINX_CONF_DIR}/nginx.d:/etc/nginx/nginx.d:ro \
         -v ${NGINX_CONF_DIR}/sites-enabled/:/etc/nginx/sites-enabled:ro \
-        -v ${NGINX_DATA_DIR}:/var/www:rw \
+        $nginx_data \
         -v ${LOGS_DIR}/${NGINX_CONTAINER_NAME}:/var/log/nginx/:rw \
         busybox /bin/true
 
     extra_volumes=""
+    if [[ $CUSTOM_VOLUMES  != "" ]]; then
+        declare -a arr=($CUSTOM_VOLUMES)
+        for volume in "${arr[@]}"; do
+           extra_volumes+=" -v $volume "
+        done    
+    fi
+
     if [ $NGINX_USE_PHP ]; then
-        extra_volumes+="--volumes-from ${PHP_SOCKET_NAME}"
+        extra_volumes+=" --volumes-from ${PHP_SOCKET_NAME} "
     fi
 
     docker run -d \
